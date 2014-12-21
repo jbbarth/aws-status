6f7d8055613c8b0542dca5185cdc46e37a96942a
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-12-21 23:26:32 +0100

    Add integration test for number of detected RSS feeds

---
+++ tests/test_status_page.py
@@ -0,0 +1,15 @@
+import subprocess
+import unittest
+
+from aws_status.status_page import StatusPage
+
+class TestStatusPage(unittest.TestCase):
+    def test_number_of_detected_feeds(self):
+        command = "curl -s http://status.aws.amazon.com/|grep rss|sort -u|wc -l"
+        p = subprocess.Popen(command, stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE,
+                                      shell=True)
+        output, errors = p.communicate()
+        expected_feeds = int(output)
+        self.assertEqual(expected_feeds, len(StatusPage().rss_urls))
+
