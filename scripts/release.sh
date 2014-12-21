9eed7dc195097a16617b46df133e9ca2e135c58a
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-12-21 21:21:23 +0100

    Improve release script (sed is weird on OSX)

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,7 +4,7 @@ version_file=aws_status/__init__.py
 echo "Current version: $(grep __version__ $version_file)"
 echo -n "What is the new version number? : "
 read version
-sed -i'' "/^__version__/s/=.*/= '$version'/" "$version_file"
+perl -pi -e "s/^__version__.*/__version__ = '$version'/" $version_file
 git commit -a -m "Bump version to $version"
 git tag $version
 git push
