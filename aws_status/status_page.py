6f92db0617145efde6b5ac3ee1d84ce38ab9d095
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-12-21 23:34:56 +0100

    Ensure python 3.x compatibility

--- aws_status/status_page.py
+++ aws_status/status_page.py
@@ -1,5 +1,12 @@
 import re
-import urllib
+
+try:
+    #python 3.x
+    from urllib.request import urlopen
+except ImportError:
+    #python 2.x
+    from urllib2 import urlopen
+
 from .feed import Feed
 
 class StatusPage(object):
@@ -22,7 +29,7 @@ class StatusPage(object):
 
     def page(self):
         if not self.page_source:
-            self.page_source = urllib.urlopen(self.BASE_URL)
+            self.page_source = urlopen(self.BASE_URL)
         return self.page_source
 
     def parse_page(self):
